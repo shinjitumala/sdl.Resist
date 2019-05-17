3692e45b65fbde07c5a27ad1fbd29e4a8a0793b4
shinjitumala <funny.pig.run@gmail.com>
2019-05-17 16:57:15 +0900

    課題の実装が完了した。

--- app/src/main/java/jp/ac/titech/itpro/sdl/resist/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/resist/MainActivity.java
@@ -9,12 +9,23 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.sql.Time;
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
     private final static String TAG = MainActivity.class.getSimpleName();
 
     private RotationView rotationView;
     private SensorManager manager;
     private Sensor gyroscope;
+
+    // storage for the last time that there was an input from the gyroscope
+    private long last_time;
+
+    // storage for getting the average of N data
+    private final int N = 2;
+    private final float alpha = (float) (N - 1) / N;
+    private float last_omega;
+    private float last_rotation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,7 +65,21 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public void onSensorChanged(SensorEvent event) {
         float omegaZ = event.values[2];  // z-axis angular velocity (rad/sec)
         // TODO: calculate right direction that cancels the rotation
-        rotationView.setDirection(omegaZ);
+        // use the average of the last N data as omegaZ
+        omegaZ = alpha * last_omega + (1 - alpha) * omegaZ;
+
+
+        long current_time = event.timestamp;
+        float current_rotation;
+
+        current_rotation = last_rotation + omegaZ * (float) (current_time - last_time) / 1000000000;
+
+        rotationView.setDirection(current_rotation);
+//        Log.d(TAG, "onSensorChanged{ omega: " + omegaZ + ", dt: " + ((float) (current_time - last_time) / 1000000) + ", dr: " + (new_rotation - current_rotation) + "}");
+
+        last_time = current_time;
+        last_omega = omegaZ;
+        last_rotation = current_rotation;
     }
 
     @Override
